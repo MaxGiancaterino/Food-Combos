aed8ccd8bfe4ec1cc11fa2b74293a0807248296e
Clemen Deng <clemen@seas.upenn.edu>
2017-09-09 18:58:40 -0400

    yes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,207 +1,121 @@
 import React from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
+  ActivityIndicator,
+  Button,
+  Dimensions,
   StyleSheet,
   Text,
-  TouchableOpacity,
+  TextInput,
   View,
 } from 'react-native';
-import { WebBrowser } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdHdmZZd0oxAVSC6vFQYK396U8VCHMLmM",
+  authDomain: "food-combos.firebaseapp.com",
+  databaseURL: "https://food-combos.firebaseio.com/",
+  storageBucket: "",
+  messagingSenderId: "751896869172"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+  state = {
+    text: '',
+    loading: false,
+  }
+
+
+  componentWillMount() {
+    let initialLoad = true;
+    this.setState({loading: true});
+
+    firebase.database().ref('example').on('value', (snapshot) => {
+      this.setState({text: snapshot.val() && snapshot.val().text});
+
+      if (initialLoad) {
+        this.setState({loading: false});
+        initialLoad = false;
+      }
+    });
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+        <Text style={styles.title}>
+          Store some value in Firebase!
+        </Text>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+        <TextInput
+          onChangeText={text => { this.setState({text}) }}
+          onSubmitEditing={this._saveValue}
+          value={this.state.text}
+          style={styles.textInput}
+        />
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+        <Button
+          onPress={this._saveValue}
+          title="Save"
+        />
 
-            <View
-              style={[
-                styles.codeHighlightContainer,
-                styles.homeScreenFilename,
-              ]}>
-              <MonoText style={styles.codeHighlightText}>
-                screens/HomeScreen.js
-              </MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-				Hello my name is grace and I am an amazing person.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity
-              onPress={this._handleHelpPress}
-              style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>
-                Help, it didn’t automatically reload!
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>
-            This is a tab bar. You can edit it in:
-          </Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>
-              navigation/MainTabNavigator.js
-            </MonoText>
-          </View>
-        </View>
+        {this._maybeRenderLoadingOverlay()}
       </View>
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use
-          useful development tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
+  _saveValue = async () => {
+    try {
+      this.setState({loading: true});
+      await firebase.database().ref('example').set({ text: this.state.text });
+    } catch(e) {
+      // Error! oh no
+    } finally {
+      this.setState({loading: false});
     }
   }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/development-mode'
-    );
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
+  _maybeRenderLoadingOverlay = () => {
+    if (this.state.loading) {
+      return (
+        <View style={[StyleSheet.absoluteFill, styles.loadingOverlay]}>
+          <ActivityIndicator
+            color="#fff"
+            animating
+            size="large"
+          />
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    justifyContent: 'center',
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  title: {
+    fontSize: 20,
   },
-  getStartedContainer: {
+  textInput: {
+    width: Dimensions.get('window').width - 30,
+    marginHorizontal: 15,
+    padding: 5,
+    borderRadius: 2,
+    borderWidth: 1,
+    borderColor: '#eee',
+    marginVertical: 15,
+    height: 50,
+    fontSize: 16,
+  },
+  loadingOverlay: {
+    backgroundColor: 'rgba(0,0,0,0.4)',
     alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+    justifyContent: 'center',
   },
 });
