a071e9a77b37eee95561963179dfb293d1786a90
gracebrentano <grace.brentano@gmail.com>
2017-09-09 13:50:13 -0400

    grace

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import RootNavigation from './navigation/RootNavigation';
-//comment
+//comment grace
 export default class App extends React.Component {
   state = {
     assetsAreLoaded: false,
